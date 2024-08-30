f24166e67614e3ac55ff96b7d913ceffd27cf1d0
yuriy.belotserkovskiy <yuriy.belotserkovskiy@redcollar.ru>
2024-08-30 11:19:17 +0300

    Add databases in local.py

--- config/settings/local.py
+++ config/settings/local.py
@@ -73,3 +73,14 @@ INSTALLED_APPS += ["django_extensions"]
 CELERY_TASK_EAGER_PROPAGATES = True
 # Your stuff...
 # ------------------------------------------------------------------------------
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
+    }
+}
